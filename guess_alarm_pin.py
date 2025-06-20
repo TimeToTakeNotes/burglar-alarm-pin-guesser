412c8a8b8d5640962ccf1323d04cffe520a14c3b
Arno Christie <arno.christie@gmail.com>
2025-06-20 18:40:51 +0200

    Solution for burglar alarm PIN guessing assessment

---
+++ guess_alarm_pin.py
@@ -0,0 +1,24 @@
+from typing import List
+from itertools import product # Generates all possible combos.
+
+class GuessAlarmPin:
+    def guess_pins(self, total_digits: int = 4) -> List[str]:
+        digits = ['5', '7']
+        pins = []
+
+        # Loop to generate all PIN combos.
+        for p in product(digits, repeat=total_digits):
+                pins.append(''.join(p))
+
+        return pins
+
+# Script entry point
+if __name__ == '__main__':
+    guesser = GuessAlarmPin()
+    possible_pins = guesser.guess_pins()
+
+    print("Possible Pins:")
+    for pin in possible_pins:
+        print(pin)
+
+    print(f'Total Number of Guesses: {len(possible_pins)}')
